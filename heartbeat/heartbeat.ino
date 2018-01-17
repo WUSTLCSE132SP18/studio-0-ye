a3862fd11533af0fd0235f234de2d78fc3ef5ffd
karenye <khye@wustl.edu>
2018-01-17 16:57:58 -0600

    Update

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -12,6 +12,4 @@ void loop() {
   delay(1000);
   digitalWrite(13, LOW);
   delay(1000);
-
-
 }
--- src/Heartbeat.java
+++ src/Heartbeat.java
@@ -3,8 +3,17 @@ public class Heartbeat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double time =0;
-		Thread.sleep(1000);
+		double time = 0;
+		while (time < 10) {
+			try {
+				Thread.sleep(100);
+				time +=0.1;
+				System.out.println("Time elapsed: " + time);
+			}
+			catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
