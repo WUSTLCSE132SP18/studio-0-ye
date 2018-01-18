3a293522ae2acb6c375813796aca01bd79e592f9
karenye <khye@wustl.edu>
2018-01-18 17:29:27 -0600

    fixed bug

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -1,6 +1,8 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
+  Serial.println("HI");
 }
 
 void loop() {
@@ -8,7 +10,7 @@ void loop() {
   int time = millis();
   Serial.print(time);
   Serial.println("sec have elapsed");
-  
+  Serial.println("hi");
   digitalWrite(13, HIGH);
   delay(500);
   digitalWrite(13, LOW);
--- helloworld/helloworld.ino
+++ helloworld/helloworld.ino
@@ -7,7 +7,14 @@
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello, world!");
+  int x = 7;
+  Serial.print("X = ");
+  Serial.println(x);
 }
 
 void loop() {
+  delay(100);
+  int time = millis();
+  Serial.print("Total time since beginning of execution = ");
+  Serial.println(time);
 }
